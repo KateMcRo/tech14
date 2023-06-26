8f373d05cb0f6a4dc8a434d4a901fe0b23359fa3
KateMcRo <katemcr0@gmail.com>
2023-06-26 14:55:39 -0700

    force

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-const express = require('express')
-const controllers = require('./controllers')
-const { engine } = require('express-handlebars')
-const connection = require('./config/connection')
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const controllers = require("./controllers");
+const { engine } = require("express-handlebars");
+const connection = require("./config/connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const path = require("path");
+require("dotenv").config();
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sessionStore = new SequelizeStore({
-    db: connection,
+  db: connection,
 });
 
-
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', './views')
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: sessionStore,
-        cookie: {
-            secure: false,
-            maxAge: 864000000,
-        },
-    })
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      secure: false,
+      maxAge: 864000000,
+    },
+  })
 );
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.json())
-app.use(controllers)
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(controllers);
 
 app.listen(PORT, async () => {
-    console.log(`Listening on PORT ${PORT}`);
-    try {
-      await sessionStore.sync();
-      console.log('Session store synced');
-    } catch (error) {
-      console.error('Unable to sync session store:', error);
-    }
-  });
+  console.log(`Listening on PORT ${PORT}`);
+  try {
+    await sessionStore.sync({
+      force: true,
+    });
+    console.log("Session store synced");
+  } catch (error) {
+    console.error("Unable to sync session store:", error);
+  }
+});
